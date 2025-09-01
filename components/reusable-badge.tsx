280bfa3be9535879ded1376a9b1c67e503e9b608
Maxi Vecchio <maximilianovecchio@gmail.com>
2025-09-01 19:18:53 -0300

    Update reusable-badge.tsx

--- components/reusable-badge.tsx
+++ components/reusable-badge.tsx
@@ -1,6 +1,6 @@
 import { Badge } from "@/components/ui/badge";
 import React, { type ReactNode } from "react";
-import {cn} from "@/lib/utils";
+import { cn } from "@/lib/utils";
 
 type ReusableBadgeProps = {
     children: ReactNode;
@@ -9,11 +9,28 @@ type ReusableBadgeProps = {
     badgeClassName?: string;
 };
 
-const ReusableBadge: React.FC<ReusableBadgeProps> = ({ children, icon, className = "", badgeClassName = "" }) => {
+const ReusableBadge: React.FC<ReusableBadgeProps> = ({
+                                                         children,
+                                                         icon,
+                                                         className = "",
+                                                         badgeClassName = "",
+                                                     }) => {
+    const Icon = icon; // <- alias en mayúscula
+
     return (
-        <div className={cn("inline-flex items-center justify-center p-1 bg-gradient-to-r from-amber-100 to-yellow-100 rounded-xl mb-2", className)}>
-            <Badge className={cn("bg-gradient-to-r h-6 from-amber-500 to-yellow-600 text-white border-0 px-4 py-2 text-sm font-medium shadow-lg", badgeClassName)}>
-                {icon && <icon className="mr-2 h-4 w-4 text-white" />}
+        <div
+            className={cn(
+                "inline-flex items-center justify-center p-1 bg-gradient-to-r from-amber-100 to-yellow-100 rounded-xl mb-2",
+                className,
+            )}
+        >
+            <Badge
+                className={cn(
+                    "bg-gradient-to-r h-6 from-amber-500 to-yellow-600 text-white border-0 px-4 py-2 text-sm font-medium shadow-lg",
+                    badgeClassName,
+                )}
+            >
+                {Icon && <Icon className="mr-2 h-4 w-4 text-white" />}
                 {children}
             </Badge>
         </div>
