cf1074a42b2404d031376aac8fbf100b6c384a71
Maxi Vecchio <maximilianovecchio@gmail.com>
2025-09-13 14:53:47 -0300

    Update calendar.tsx

--- components/ui/calendar.tsx
+++ components/ui/calendar.tsx
@@ -42,7 +42,7 @@ function Calendar({
         ),
         day: cn(
           buttonVariants({ variant: "ghost" }),
-          "size-8 p-0 font-normal aria-selected:opacity-100 w-full"
+          "cursor-pointer size-8 p-0 font-normal aria-selected:opacity-100 w-full"
         ),
         day_range_start:
           "day-range-start aria-selected:bg-primary aria-selected:text-primary-foreground",
